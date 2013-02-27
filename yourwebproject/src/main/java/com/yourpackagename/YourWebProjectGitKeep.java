0d916675b6f02a7519d7a76d4a9f94b6d871b8c1
Efren Augusto Garcia <efren.gl@gmail.com>
2013-02-26 23:22:31 -0500

    prueba

--- yourwebproject/src/main/java/com/yourpackagename/YourWebProjectGitKeep.java
+++ yourwebproject/src/main/java/com/yourpackagename/YourWebProjectGitKeep.java
@@ -7,5 +7,5 @@ package com.yourpackagename;
  * @created: 7/7/12 2:45 PM
  * @company: &copy; 2012, Kaleidosoft Labs
  */
-public class YourWebProjectGitKeep { 
+public class YourWebProjectGitKeep {  
 } 
