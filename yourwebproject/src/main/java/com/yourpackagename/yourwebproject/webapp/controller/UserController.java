5193fb12ccfac04ad741d3e50309aa7d50ea86f9
efrengarcial <efren.gl@gmail.com>
2013-04-23 21:54:25 +0000

    from seguros bolivar

--- yourwebproject/src/main/java/com/yourpackagename/yourwebproject/webapp/controller/UserController.java
+++ yourwebproject/src/main/java/com/yourpackagename/yourwebproject/webapp/controller/UserController.java
@@ -2,17 +2,11 @@ package com.yourpackagename.yourwebproject.webapp.controller;
 
 import java.util.Locale;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
